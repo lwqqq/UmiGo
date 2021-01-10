db97ac23c8b77fee9c7f32969c491bbab06de93f
lwqq3 <lwq@lwqq3deMacBook-Pro.local>
2021-01-11 00:10:41 +0800

    fix

--- Go/gotypes.py
+++ Go/gotypes.py
@@ -25,10 +25,5 @@ class Point(namedtuple('Point', 'row col')):
         ]
 
 
-class Bored():
-    def __init__(self, num_rows, num_cols):
-        self.num_rows = num_rows
-        self.num_cols = num_cols
-        self._grid = {}
 
 
